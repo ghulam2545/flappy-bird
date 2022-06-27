f0c2bd597fee147e553fd7956668fcccfc78fbad
ghulam2545 <ghulam2545@gmail.com>
2022-06-28 00:57:37 +0530

    flying done, kindly genrate pipes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+
+
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <ctime>
@@ -31,29 +33,22 @@ namespace gh {
     class Texture {
        public:
         void load();
+        sf::Texture pipe;
 
        protected:
         std::vector<sf::Texture> birds;
         sf::Texture background;
-        sf::Texture pipe;
+
         sf::Texture gameOver;
         sf::Texture title;
     };
 
     class Bird : protected Texture {
        public:
-        void load() {
-            sprite.setPosition(250, 300);
-            sprite.setScale(1.8, 1.8);
-        }
-
-        void draw(sf::RenderWindow& window) { window.draw(sprite); }
-        void update(int whichOne) {
-            gh::Texture::load();
-            sprite.setTexture(birds[whichOne]);
-            sprite.setPosition(250, 320);
-            sprite.setScale(1.6, 1.6);
-        }
+        void load();
+        void draw(sf::RenderWindow& window);
+        void update(int whichOne);
+        std::vector<float> getPos();
 
         sf::Sprite sprite;
         double speed;
@@ -61,6 +56,36 @@ namespace gh {
     };
 
     enum GameState { playing, not_playing, game_over };
+
+    class Pipe : protected Texture {
+       public:
+        std::vector<sf::Sprite>& genratePipe(gh::GameState& state, int gameFrameCount) {
+            gh::Texture::load();
+            if (state == gh::playing && gameFrameCount % 110 == 0) {
+                int len = rand() % 275 + 75;
+                int gap = 180;  // vertical gap between pipes
+
+                // lower pipe
+                sf::Sprite lowerPipe;
+                lowerPipe.setTexture(gh::Texture::pipe);
+                lowerPipe.setPosition(900, len + gap);
+                lowerPipe.setScale(1, 2);
+
+                // upper pipe
+                sf::Sprite upperPipe;
+                upperPipe.setTexture(gh::Texture::pipe);
+                upperPipe.setPosition(900, len);
+                upperPipe.setScale(1, -2);
+
+                pipes.push_back(lowerPipe);
+                pipes.push_back(upperPipe);
+            }
+            return pipes;
+        }
+
+       private:
+        std::vector<sf::Sprite> pipes;
+    };
 
     class Game : protected Texture {
        public:
@@ -106,25 +131,65 @@ int main() {
     gh::Bird bird;
     bird.load();
 
+    gh::Pipe pipe;
+
     gh::Game app;
     app.load();
 
     while (window.isOpen()) {
         bird.update(0);
 
-        if (app.gameFrameCount % 6 == 0) {
-            ++bird.birdFrameCount;
-        }
-        if (bird.birdFrameCount == 3) {
-            bird.birdFrameCount = 0;
+        if (app.state == gh::playing) {
+            if (app.gameFrameCount % 3 == 0) {
+                ++bird.birdFrameCount;
+            }
+            if (bird.birdFrameCount == 3) {
+                bird.birdFrameCount = 0;
+            }
         }
 
         bird.update(bird.birdFrameCount);
 
-        // TODO:..................................
+        if (app.state == gh::playing) {
+            bird.sprite.move(0, bird.speed);
+            bird.speed += 0.5;
+        }
+
+        auto currPos = bird.getPos();
+        if (app.state == gh::playing) {
+            if (currPos[1] < 0) {
+                bird.sprite.setPosition(250, 0);
+                bird.speed = 0;
+            }
+            if (currPos[1] > 600) {
+                bird.speed = 0;
+                app.state = gh::game_over;
+                sound.playHit();
+            }
+        }
+
+        // TODO :
+        // auto pipes = pipe.genratePipe(app.state, app.gameFrameCount);
+
+        // move pipes
         // if (app.state == gh::playing) {
-        //     bird.sprite.move(0, bird.speed);
-        //     bird.speed += 0.5;
+        //     for (auto& e : pipes) {
+        //         e.move(-3, 0);
+        //     }
+        // }
+
+        // remove pipes if offscreen
+        // if (app.gameFrameCount % 100 == 0) {
+        //     auto startitr = pipes.begin();
+        //     auto enditr = pipes.begin();
+
+        //     for (; enditr != pipes.end(); enditr++) {
+        //         if ((*enditr).getPosition().x > -104) {
+        //             break;
+        //         }
+        //     }
+
+        //     pipes.erase(startitr, enditr);
         // }
 
         sf::Event event;
@@ -132,29 +197,33 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 // close the window
                 window.close();
-            } else if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::Space) {
-                    if (app.state == gh::not_playing) {
-                        app.state = gh::playing;
-                    }
-                    if (app.state == gh::playing) {
-                        bird.speed = -8;
-                        sound.playFly();
-                    }
-                } else if (event.key.code == sf::Keyboard::Escape && app.state == gh::game_over) {
-                    bird.load();
-                    app.score = 0;
-                    // clear pipes...
+            } else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space) {
+                if (app.state == gh::not_playing) {
+                    app.state = gh::playing;
                 }
+                if (app.state == gh::playing) {
+                    bird.speed = -8;
+                    sound.playFly();
+                }
+            } else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape && app.state == gh::game_over) {
+                bird.load();
+                app.score = 0;
+                // clear pipes...
             }
         }
 
         window.clear();
-        // app.drawBackground(window);
-        // app.drawTitle(window);
-        // app.drawGameOver(window);
-        // app.drawScore(window);
+        app.drawBackground(window);
+        if (app.state == gh::not_playing) {
+            app.drawTitle(window);
+        }
+        if (app.state == gh::game_over) {
+            app.drawGameOver(window);
+        }
+        app.drawScore(window);
         bird.draw(window);
+        // for (auto& e : pipes) window.draw(e);
+
         window.display();
 
         ++app.gameFrameCount;
@@ -197,6 +266,28 @@ namespace gh {
         title.loadFromFile("./assets/logo.png");
     }
 
+    void Bird::load() {
+        sprite.setPosition(250, 300);
+        sprite.setScale(1.6, 1.6);
+    }
+
+    void Bird::draw(sf::RenderWindow& window) { window.draw(sprite); }
+
+    void Bird::update(int whichOne) {
+        gh::Texture::load();
+        sprite.setTexture(birds[whichOne]);
+    }
+
+    std::vector<float> Bird::getPos() {
+        auto x = sprite.getPosition().x;
+        auto y = sprite.getPosition().y;
+        auto width = 34 * sprite.getScale().x;
+        auto height = 24 * sprite.getScale().y;
+
+        std::vector<float> res = {x, y, width, height};
+        return res;
+    }
+
     void Game::load() {
         font.loadFromFile("./assets/flappy.ttf");
 
@@ -213,7 +304,7 @@ namespace gh {
         this->title.setPosition(250, 110);
 
         this->gameOver.setTexture(gh::Texture::gameOver);
-        this->gameOver.setPosition(270, 200);
+        this->gameOver.setPosition(270, 150);
         this->gameOver.setScale(1.8, 1.8);
 
         scoreText.setFont(font);
@@ -226,7 +317,7 @@ namespace gh {
         highScoreText.setFillColor(sf::Color::White);
         highScoreText.setCharacterSize(25);
         highScoreText.setString("HIGH SCORE : " + std::to_string(highScore));
-        highScoreText.setPosition(15, 60);
+        highScoreText.setPosition(15, 55);
 
         startText.setFont(font);
         startText.setFillColor(sf::Color::White);
@@ -241,14 +332,16 @@ namespace gh {
         }
     }
 
-    void Game::drawTitle(sf::RenderWindow& window) { window.draw(this->title); }
+    void Game::drawTitle(sf::RenderWindow& window) {
+        window.draw(this->title);
+        window.draw(startText);
+    }
 
     void Game::drawGameOver(sf::RenderWindow& window) { window.draw(this->gameOver); }
 
     void Game::drawScore(sf::RenderWindow& window) {
         window.draw(scoreText);
         window.draw(highScoreText);
-        window.draw(startText);
     }
 
 }  // namespace gh
